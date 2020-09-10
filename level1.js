9ae9c7b502d7747cc37b2a728d7f56ae074b28dd
SubhojitGhosh-123456789 <69459860+SubhojitGhosh-123456789@users.noreply.github.com>
2020-09-10 21:14:20 +0530

    Add files via upload

---
+++ level1.js
@@ -0,0 +1,19 @@
+class Level1 {
+    constructor(x,y) {
+      var options = {
+          isStatic: false,
+          restitution:0.8,
+          friction:1.0,
+          density:1.0
+      }
+      this.body = Bodies.rectangle(x,y,30,50,options);
+
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill(79,160,249);
+      rect(pos.x, pos.y, 30, 50);
+    }
+  };
